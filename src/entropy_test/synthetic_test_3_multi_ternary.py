12e51befdaca9a9b4869dbbd9478187b4cf74438
saeyoung <littlebee6@naver.com>
2020-01-11 10:30:44 -0800

    entropy test updated

--- src/entropy_test/synthetic_test_3_multi_ternary.py
+++ src/entropy_test/synthetic_test_3_multi_ternary.py
@@ -17,68 +17,6 @@ from sklearn.linear_model import LinearRegression
 
 from utils import *
 
-# regression model for a fixed input string size N
-def regression_model_ternary(size, number_of_p=30, verbose=False):
-    ratio_list =[]
-    true_entropy = []
-    
-    probabilities=[]
-    for i in range(number_of_p):
-        p1= np.random.uniform(0,1,1)[0]
-        p2 = np.random.uniform(0,1,1)[0]
-        p3 = np.random.uniform(0,1,1)[0]
-        
-        probabilities.append([p1,p2,p3]/(p1+p2+p3))
-            
-    for p in probabilities:
-        true_entropy.append(entropy(p))
-        uncompressed = list_to_string(multinomial(size, p))
-        compressed = compress(uncompressed)
-        compression_ratio = len(compressed)/len(uncompressed)
-        ratio_list.append(compression_ratio)
-        
-        if verbose:
-            print("p : ", p)
-            print("theoretical entropy: ", entropy(p))
-            print("compression ratio: ", compression_ratio)
-            print()
-
-    # linear regression
-    reg = LinearRegression().fit(np.array(true_entropy[:]).reshape(-1, 1), np.array(ratio_list[:]))
-    print("y = ax + b model")
-    print("a = ", reg.coef_)
-    print("b = ", reg.intercept_)
-
-    plt.scatter(true_entropy, ratio_list, marker='.', label = "LZW compressor")
-    plt.plot(true_entropy, reg.predict(np.array(true_entropy).reshape(-1,1)), label="regression", color="orange")
-
-    plt.title("Compression Ratio for Multinomial(3,p) with Size {}".format(size))
-    plt.xlabel("theoretical entropy")
-    plt.ylabel("compression ratio")
-    plt.legend()
-    plt.show()
-
-    return reg, ratio_list, true_entropy
-
-def get_entropy(size, compression_ratio):
-    # mapping compression ratio to entropy
-    reg, ratio_list, true_entropy = regression_model_ternary(size, number_of_p=30, verbose=False)
-    reg_inv = LinearRegression().fit(np.array(ratio_list[:]).reshape(-1, 1), np.array(true_entropy[:]))
-    ent = reg_inv.predict(np.array(compression_ratio).reshape(-1, 1))
-
-    plt.scatter(true_entropy, ratio_list, marker='.')
-    plt.plot(true_entropy, reg.predict(np.array(true_entropy).reshape(-1,1)), label="regression", color="orange")
-    plt.axvline(ent, color="grey", alpha=0.5)
-    plt.axhline(compression_ratio, color="grey", alpha=0.5)
-    plt.scatter(ent, compression_ratio, color="red")
-
-    plt.title("Compression Ratio for Multinomial(3,p) with Size {}".format(size))
-    plt.xlabel("theoretical entropy")
-    plt.ylabel("compression ratio")
-    plt.legend()
-    plt.show()
-    return ent
-
 def g(p):
     return entropy([p,1-p]) + p
 
@@ -100,39 +38,59 @@ def g_inverse(H, a=0.001):
     
     return p_hat
 
-def test():
-    #### edit here ####
-    power = 10
-    size = 2 ** power
-    p = [1/6, 2/3, 1/6]
-    ###################
-
+def test(size, name, plot, verbose):
+    p = random_p(n=3)
+    
     # sample binary string
     print("size: ", size)
     print("p   : ", p)
     uncompressed = multinomial(size, p)
     p_tilda = [np.mean(np.array(uncompressed)==0), np.mean(np.array(uncompressed)==1), np.mean(np.array(uncompressed)==2)]
 
+    # compression
     uncompressed = list_to_string(uncompressed)
     compressed = compress(uncompressed)
-    
-    # len()
     compression_ratio = len(compressed)/len(uncompressed)
-    print("Compression ratio: ", compression_ratio)
-    print()
 
     # entropy
-    estimated_ent = get_entropy(size, compression_ratio)
+    estimated_ent = get_entropy_ternary(size, compression_ratio, name, plot=True)
+    theoretical_ent = entropy(p)
+    empirical_ent = entropy(p_tilda)
+
+    print("Compression ratio: ", compression_ratio)
     print("Estimated entropy: ", estimated_ent)
-    print("Theoretical entropy: ", entropy(p))
-    print("Empirical entropy: ", entropy(p_tilda))
+    print("Theoretical entropy: ", theoretical_ent)
+    print("Empirical entropy: ", empirical_ent)
+
+    return compression_ratio, estimated_ent, theoretical_ent, empirical_ent
 
 def main():
+    #### edit here ####
+    power = 10
+    size = 2 ** power
+    name = "a ternary multinomial process"
+    plot = True
+    verbose = True
+    ###################
     print("*******************************************************")
     print("*******************************************************")
     print("********** Running the Testing Scripts. ***************")
 
-    test()
+    theo_ent = []
+    est_ent = []
+    for num in range(2):
+        compression_ratio, estimated_ent, theoretical_ent, empirical_ent = test(size, name, plot, verbose)
+        theo_ent.append(theoretical_ent)
+        est_ent.append(estimated_ent)
+
+    error = np.abs(np.array(theo_ent) - np.array(est_ent))
+    print(np.mean(error))
+    plt.xlabel("Absolute discrepancy between theoretical and estimated entropy")
+    plt.title("Error distribution, string length={}".format(size))
+    plt.hist(error)
+    plt.axvline(np.mean(error), color="red", label="mean={}".format(np.mean(error).round(3)))
+    plt.legend()
+    plt.show()
 
     print("********** Testing Scripts Done. **********************")
     print("*******************************************************")
